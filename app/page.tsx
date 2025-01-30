edcb34a70818d68a414e97779b27e1e4f91993a2
sarim <sarimali@live.com>
2025-01-30 05:57:29 +0500

    localization

--- app/page.tsx
+++ app/page.tsx
@@ -25,6 +25,7 @@ export default function Home() {
           <HeroSection />
         </section>
         <FeaturedProducts />
+        
         <ProductShowcase />
         <Testimonials />
         <AboutUs />
